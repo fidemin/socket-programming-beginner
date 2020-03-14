ba3d70a3b60505b6c37c02367ce501107d587d8a
Yunhong Min <yhmin84@gmail.com>
2020-03-14 20:33:51 +0900

    pthread join

---
+++ theory/pthread_join_func.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void * do_sum(void *data) {
+	int i;
+	int sum;
+
+	int max = *((int *) data);
+
+	for (i = 1; i <= max; i++) {
+		sum = sum + i;
+		printf("%d - Add %d\n", max, i);
+		sleep(1);
+	}
+	printf("sum from 1 to %d - %d\n", max, sum);
+}
+
+int main() {
+	int thr_id;
+	pthread_t p_thread[3];
+
+	int status;
+	int a = 4;
+	int b = 5;
+	int c = 6;
+
+	thr_id = pthread_create(&p_thread[0], NULL, do_sum, (void *)&a);
+	thr_id = pthread_create(&p_thread[1], NULL, do_sum, (void *)&b);
+	thr_id = pthread_create(&p_thread[2], NULL, do_sum, (void *)&c);
+	
+	pthread_join(p_thread[0], (void**) &status);
+	pthread_join(p_thread[1], (void**) &status);
+	pthread_join(p_thread[2], (void**) &status);
+
+	return 0;
+}
+
+
